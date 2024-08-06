01ed57b0b7d5f4a868626832586fd56f1eb581bd
moshe-druk <dmd6708713@gmail.com>
2024-08-06 11:38:13 +0300

    add try to Task and read file

---
+++ GetDataAsync.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attackserver
+{
+    internal class GetDataAsync
+    {
+        public GetDataAsync() { }
+        public static async Task<string> GetDataAsync_fun()
+        {
+            string path = @"C:\Users\admin\source\repos\Attackserver\TextFile1.txt";
+            string res = await Task.Run(() => File.ReadAllText(path));
+            return res;
+        }
+        
+        public static async Task<string> GetReadDataAsync_A() 
+        {
+            await Task.Delay(1000);
+            return "data from A";
+        }
+        public static async Task<string> GetReadDataAsync_B()
+        {
+            await Task.Delay(2000);
+            return "data from B";
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Attackserver
-{
+{    
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            Task<string> taskA = GetDataAsync.GetReadDataAsync_A();
+            Task<string> taskB = GetDataAsync.GetReadDataAsync_B();
+
+            await Task.WhenAll(taskA, taskB); 
+            
+            string res =  await GetDataAsync.GetDataAsync_fun();
+            Console.WriteLine(res);
+
+            Console.ReadLine();
         }
     }
 }
